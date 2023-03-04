3665f90b5552073801245463b76981a15a7d96b7
Jesus <jesus.cassano.2006@gmail.com>
2023-03-05 00:16:30 +0100

    ✅ Create router file

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,20 +1,37 @@
-import { Router } from 'express';
+import { Router as router } from 'express';
 import { UsersController } from '../controller/users.controller.js';
+import { logged } from '../interceptors/logged.js';
 import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
+import createDebug from 'debug';
 
-// eslint-disable-next-line new-cap
-export const usersRouter = Router();
-// File Repo previous const repo = new UsersFileRepo();
-const repo = UsersMongoRepo.getInstance();
-const controller = new UsersController(repo);
+const debug = createDebug('W7CH5:router');
 
-usersRouter.get('/', controller.getAll.bind(controller));
+export const usersRouter = router();
+const repoUsers = UsersMongoRepo.getInstance();
+const controller = new UsersController(repoUsers);
+
+debug('Users Router');
+
 usersRouter.post('/register', controller.register.bind(controller));
 usersRouter.post('/login', controller.login.bind(controller));
-/*  TEMP
-usersRouter.patch('/addFriends/:id', controller.addFriends.bind(controller));
-usersRouter.patch('/addEnemies/:id', controller.addEnemies.bind(controller));
-usersRouter.patch('/deleteFriends/:id', controller.deleteFriends.bind(controller));
-usersRouter.patch('/deleteEnemies/:id', controller.deleteEnemies.bind(controller));
-
-*/
+usersRouter.get('/', logged, controller.getAll.bind(controller));
+usersRouter.patch(
+  '/add_friends/:id',
+  logged,
+  controller.addFriends.bind(controller)
+);
+usersRouter.patch(
+  '/remove_friends/:id',
+  logged,
+  controller.removeFriends.bind(controller)
+);
+usersRouter.patch(
+  '/add_enemies/:id',
+  logged,
+  controller.getAll.bind(controller)
+);
+usersRouter.patch(
+  '/remove_enemies/:id',
+  logged,
+  controller.addFriends.bind(controller)
+);
