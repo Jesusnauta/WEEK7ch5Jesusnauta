b6f44acbd86e46309d15b3e67b22ba52472533a7
Jesus <jesus.cassano.2006@gmail.com>
2023-03-04 16:31:42 +0100

    🐛 Modify users.mongo file

--- src/repository/users.mongo.model.ts
+++ src/repository/users.mongo.model.ts
@@ -5,12 +5,10 @@ const userSchema = new Schema<User>({
   email: {
     type: String,
     required: true,
-    unique: true,
   },
   password: {
     type: String,
     required: true,
-    unique: true,
   },
   friends: [
     {
