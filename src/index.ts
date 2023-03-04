dfd7fec2994c7172bc34f3f348ee344d88be2d0e
Jesus <jesus.cassano.2006@gmail.com>
2023-03-04 16:18:03 +0100

    ✅ Add index file

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import createDebug from 'debug';
+import http from 'http';
+import { app } from './app.js';
+import { dbConnect } from './db/db.connect.js';
+// TEM import { dbConnect } from './db/db.connect.js';
+const debug = createDebug('W7CH5:index');
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer(app);
+
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('DB:', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => server.emit('error', error));
+
+server.on('error', (error) => {
+  debug('Server error:', error.message);
+});
+
+server.on('listening', () => {
+  debug('Listening in http://localhost:' + PORT);
+});
