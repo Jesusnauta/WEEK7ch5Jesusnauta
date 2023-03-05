303408745234b0e25991d1298bd570e24c244230
Jesus <jesus.cassano.2006@gmail.com>
2023-03-05 19:13:00 +0100

    ✅ Add users.mongo.repo.test file

--- src/repository/users.mongo.repo.test.ts
+++ src/repository/users.mongo.repo.test.ts
@@ -1,109 +1,109 @@
-import { User } from '../entities/users';
+import { UserStructure } from '../entities/user';
 import { UserModel } from './users.mongo.model';
 import { UsersMongoRepo } from './users.mongo.repo';
 
-jest.mock('./users.mongo.model');
+jest.mock('./users.mongo.model.js');
 
-describe('Given the UserRepo class', () => {
+describe('Given UsersMongoRepo repository', () => {
   const repo = UsersMongoRepo.getInstance();
-  describe('When it is called', () => {
-    test('Then it should be instantiated', () => {
+
+  const mockPopulateFunction = (mockPopulateValue: unknown) => ({
+    populate: jest.fn().mockImplementation(() => ({
+      populate: jest.fn().mockResolvedValue(mockPopulateValue),
+    })),
+  });
+
+  describe('When the repository is instanced', () => {
+    test('Then, the repo should be instance of UsersMongoRepo', () => {
       expect(repo).toBeInstanceOf(UsersMongoRepo);
     });
   });
 
-  describe('When query method is called', () => {
-    test('Then it should return a array', async () => {
-      const mockUser = [{ user: 'test' }];
-      (UserModel.find as jest.Mock).mockImplementation(() => ({
-        populate: jest.fn().mockResolvedValue(mockUser),
-      }));
-
+  describe('When the query method is used', () => {
+    test('Then it should return the result of the users', async () => {
+      const mockPopulateValue = [{ id: '1' }, { id: '2' }];
+      (UserModel.find as jest.Mock).mockImplementation(() =>
+        mockPopulateFunction(mockPopulateValue)
+      );
       const result = await repo.query();
-
-      expect(UserModel.find).toHaveBeenCalled();
-      expect(result).toEqual(mockUser);
+      expect(result).toEqual([{ id: '1' }, { id: '2' }]);
     });
   });
 
-  describe('When i use queryId', () => {
-    beforeEach(async () => {
-      (UserModel.findById as jest.Mock).mockImplementation(() => ({
-        populate: jest.fn().mockResolvedValue({ id: '1' }),
-      }));
-    });
-    test('Then should return the selected data', async () => {
+  describe('When the queryId method is used', () => {
+    test('Then if the findById method resolve value to an object, it should return the object', async () => {
+      const mockPopulateValue = { id: '1' };
+      (UserModel.findById as jest.Mock).mockImplementation(() =>
+        mockPopulateFunction(mockPopulateValue)
+      );
+
       const result = await repo.queryId('1');
       expect(UserModel.findById).toHaveBeenCalled();
       expect(result).toEqual({ id: '1' });
     });
-    test('Then if the findById method resolve value to undefined, it should throw an Error', async () => {
-      (UserModel.findById as jest.Mock).mockImplementation(() => ({
-        populate: jest.fn().mockResolvedValue(null),
-      }));
-      expect(async () => repo.queryId('3')).rejects.toThrow();
+
+    test('Then if the findById method resolve value to null, it should throw an Error', async () => {
+      const mockPopulateValue = null;
+      (UserModel.findById as jest.Mock).mockImplementation(() =>
+        mockPopulateFunction(mockPopulateValue)
+      );
+
+      expect(async () => repo.queryId('')).rejects.toThrow();
     });
   });
 
-  describe('When search method is called', () => {
-    test('Then if the id is found it should return an array', async () => {
-      const mockUser = [{ key: 'test', value: '23' }];
-      (UserModel.find as jest.Mock).mockResolvedValue(mockUser);
+  describe('When the search method is used', () => {
+    test('Then if it has an mock query object, it should return find resolved value', async () => {
+      const mockPopulateValue = [{ id: '1' }];
+      (UserModel.find as jest.Mock).mockImplementation(() =>
+        mockPopulateFunction(mockPopulateValue)
+      );
 
-      const result = await repo.search({ key: 'test', value: '23' });
-
-      expect(UserModel.find).toHaveBeenCalled();
-      expect(result).toEqual(mockUser);
-    });
-    test('Then the id is NOT found should throw error', async () => {
-      (UserModel.find as jest.Mock).mockResolvedValue(null);
-      expect(async () =>
-        repo.search({ key: 'testno', value: '25' })
-      ).rejects.toThrow();
+      const mockQuery = { key: 'test', value: 'test' };
+      const result = await repo.search(mockQuery);
+      expect(result).toEqual([{ id: '1' }]);
     });
   });
 
-  describe('When i use create', () => {
-    test('Then it should create a new User', async () => {
-      (UserModel.create as jest.Mock).mockResolvedValue({ name: 'test' });
+  describe('When the create method is used', () => {
+    test('Then if it has an object to create, it should return the created object', async () => {
+      (UserModel.create as jest.Mock).mockResolvedValue({ email: 'test' });
 
-      const result = await repo.create({ name: 'test' });
+      const result = await repo.create({ email: 'test' });
       expect(UserModel.create).toHaveBeenCalled();
-      expect(result).toEqual({ name: 'test' });
+      expect(result).toEqual({ email: 'test' });
     });
   });
 
   describe('When the update method is used', () => {
-    beforeEach(async () => {
-      (UserModel.findByIdAndUpdate as jest.Mock).mockResolvedValue({ id: '2' });
-    });
+    const mockUser = {
+      email: 'test',
+    } as Partial<UserStructure>;
 
-    test('Then if it has an object to update, it should return the updated object', async () => {
-      const mockUser = {
-        id: '2',
-        name: 'test',
-      } as Partial<User>;
+    test('Then if the findByIdAndUpdate method resolve value to an object, it should return the object', async () => {
+      const mockPopulateValue = { email: 'test' };
+      (UserModel.findByIdAndUpdate as jest.Mock).mockImplementation(() =>
+        mockPopulateFunction(mockPopulateValue)
+      );
 
       const result = await repo.update(mockUser);
-      expect(UserModel.find).toHaveBeenCalled();
-      expect(result).toEqual({ id: '2' });
+      expect(UserModel.findByIdAndUpdate).toHaveBeenCalled();
+      expect(result).toEqual({ email: 'test' });
     });
-    test('Then if the findByIdAndUpdate method resolve value to undefined, it should throw an Error', async () => {
-      const mockUser = {
-        id: '2',
-        name: 'test',
-      } as Partial<User>;
-      (UserModel.findByIdAndUpdate as jest.Mock).mockResolvedValue(null);
+
+    test('Then if the findByIdAndUpdate method resolve value to null, it should throw an Error', async () => {
+      const mockPopulateValue = null;
+      (UserModel.findByIdAndUpdate as jest.Mock).mockImplementation(() =>
+        mockPopulateFunction(mockPopulateValue)
+      );
+
       expect(async () => repo.update(mockUser)).rejects.toThrow();
     });
   });
 
   describe('When the delete method is used', () => {
-    beforeEach(async () => {
-      (UserModel.findByIdAndDelete as jest.Mock).mockResolvedValue({});
-    });
-
     test('Then if it has an object to delete with its ID, the findByIdAndDelete function should be called', async () => {
+      (UserModel.findByIdAndDelete as jest.Mock).mockResolvedValue({});
       await repo.destroy('1');
       expect(UserModel.findByIdAndDelete).toHaveBeenCalled();
     });
